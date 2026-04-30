a4f90aed746a322daf7404fa9f0b7e449b1c3c1b
Shivam Maurya <shivammaurya0248@gmail.com>
2026-04-30 11:20:33 +0530

    Added logging and passed constant part for the Weight serial port

--- ACFG04/weight.py
+++ ACFG04/weight.py
@@ -13,11 +13,11 @@ if not os.path.isdir("./logs"):
 # logging.config.fileConfig('logging.config')
 log = logging.getLogger('UPS_log')
 
-ports = serial.tools.list_ports.comports()
-port = [p.device for p in ports if "USB" in p.hwid or "ttyUSB" in p.device]
-print(port)
-PORT_WT = port[0]
-#PORT_WT = '/dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0'
+# ports = serial.tools.list_ports.comports()
+# port = [p.device for p in ports if "USB" in p.hwid or "ttyUSB" in p.device]
+# print(port)
+# PORT_WT = port[0]
+PORT_WT = '/dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0'
 log.info(PORT_WT)
 wt_ser = serial.Serial(
     port=PORT_WT,
@@ -38,12 +38,12 @@ def read_weight():
         wt_ser.flushInput()
         wt_ser.flush()
         weight = wt_ser.read_until()
-        print(f"Got data --- {weight}")
+        log.info(f"Got data --- {weight}")
         # eight = weight.decode("utf-8")
         weight = weight.decode('utf-8', errors='replace')
         weight = re.sub(r"[^\d\.]", "", weight)
         weight = float(weight)
-        print(f"Got weight data --- {weight*10}")
+        log.info(f"Got weight data --- {weight*10}")
         return weight
     except Exception:
         try:
@@ -70,10 +70,10 @@ def read_weight():
             weight = weight.decode("utf-8")
             weight = re.sub(r"[^\d\.]", "", weight)
             weight = float(weight)
-            print(f"Got weight data --- {weight*10}")
+            log.info(f"Got weight data --- {weight*10}")
             return weight
         except Exception as e:
-            print(f'ERROR: {e} Error in opening weight serial port')
+            log.error(f'ERROR: {e} Error in opening weight serial port')
             return 0
         
 
